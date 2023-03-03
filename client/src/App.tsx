287560e5cf4a56f09323472b635dc15835a5af2c
hamza ali <tanolihamzaali@gmail.com>
2023-03-04 01:25:41 +0500

    Configur tailwind css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1 className="text-[yellow]">Hello </h1>
+		</div>
+	);
 }
 
-export default App
+export default App;
