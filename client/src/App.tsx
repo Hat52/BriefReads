a3b06275ea1e9b094e29b67c27558517f51a9e60
hamza ali <tanolihamzaali@gmail.com>
2023-03-04 01:44:45 +0500

    feat: navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,13 @@ import './App.css';
 
 function App() {
 	return (
-		<div className="App">
-			<h1 className="text-[yellow]">Hello </h1>
+		<div className="">
+			<div className="main-container flex flex-col gap-10 px-5">
+				<div className="flex justify-center items-center w-full h-[70px] border-b border-outline">
+					<h1 className="text-typography text-2xl">BriefBooks</h1>
+				</div>
+				<div className="w-full h-[300px]"></div>
+			</div>
 		</div>
 	);
 }
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				typography: '#EEEEEE',
+				primary: '#222831',
+				secondary: '#393E46',
+				outline: '#00ADB5'
+			}
+		}
 	},
 	plugins: []
 };
