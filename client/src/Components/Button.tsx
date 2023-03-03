8d3fc5c6aff51633923228ba5b7a0731e8f550cb
hamza ali <tanolihamzaali@gmail.com>
2023-03-04 02:07:41 +0500

    update: Button component

--- client/src/Components/Button.tsx
+++ client/src/Components/Button.tsx
@@ -1,7 +1,20 @@
-export default function Button() {
+import { ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface IButton {
+	children: ReactNode;
+	onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+	className?: string;
+}
+
+export default function Button({ children, onClick, className }: IButton) {
+	const buttonClass = twMerge(
+		'w-[100px] h-[48px] bg-outline rounded-md font-medium text-typography',
+		className
+	);
 	return (
-		<button className="w-[100px] h-[48px] bg-outline rounded-md font-medium text-typography">
-			Search
+		<button onClick={onClick} className={buttonClass}>
+			{children}
 		</button>
 	);
 }
