6b210636b37ad2643caa5e881c867f6674666c97
hamza ali <tanolihamzaali@gmail.com>
2023-03-05 02:20:40 +0500

    Show your work summary added

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -2,11 +2,22 @@ import { IPreview } from '../model';
 
 export const Books: IPreview[] = [
 	{
-		name: 'Book1',
+		name: 'Show Your Works',
 		publishedDate: 'May-2023',
-		description:
-			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-		author: 'Hamza Ali'
+		description: `"Show Your Work" is a book that encourages creative individuals to share their work and ideas with the world at every stage of the creative process. Kleon argues that the traditional model of self-promotion, where an artist produces work in secret and then presents it fully formed to the public, is outdated and ineffective. Instead, he advocates for a process of "showing your work" - that is, sharing your work and ideas with others as you create them.
+
+      One of the key themes of the book is the importance of vulnerability in your creative work. Kleon argues that showing your work can be scary, but it's important to be open and honest about your process and your struggles. He suggests that by being vulnerable, you can build deeper connections with your audience and create work that resonates more strongly with others.
+      
+      Kleon also emphasizes the value of sharing your influences and inspirations with others. He encourages readers to be open about the books, music, art, and other media that inspire them and to share that inspiration with others. By doing so, you can create a community of like-minded individuals who will support and inspire you.
+      
+      Throughout the book, Kleon provides practical advice on how to show your work effectively, including tips on using social media, building an audience, and collaborating with others. He emphasizes the importance of being authentic and genuine in your interactions with others, and he provides numerous examples of individuals and organizations who have successfully used the "show your work" approach to build their brands and reach new audiences.
+      
+      One of the strengths of "Show Your Work" is its accessibility. The book is written in a clear and concise manner, with plenty of examples and anecdotes to illustrate Kleon's points. It's easy to understand and the advice is actionable, making it a practical guide for anyone who wants to get their creative work noticed.
+      
+      Another strength of the book is its relevance to a wide range of creative individuals, including writers, artists, musicians, and designers. Kleon's advice is applicable to anyone who wants to share their work and ideas with the world.
+      
+      Overall, "Show Your Work" is an inspiring and practical guide for creative individuals who want to get their work noticed. While it's difficult to summarize all the advice and insights that Kleon provides in the book, the key takeaway is that the process of "showing your work" is a powerful tool for building a community of supporters and reaching a wider audience.`,
+		author: 'Austin Kleon'
 	},
 	{
 		name: 'Book2',
