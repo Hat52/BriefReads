c63432564612e68e6fd88c82c2c553e62970267d
hamza ali <tanolihamzaali@gmail.com>
2023-03-05 01:47:10 +0500

    Add: Books dummy data

---
+++ client/src/constants/index.ts
@@ -0,0 +1,60 @@
+import { IPreview } from '../model';
+
+export const Books: IPreview[] = [
+	{
+		name: 'Book1',
+		publishedDate: 'May-2023',
+		description:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+		author: 'Hamza Ali'
+	},
+	{
+		name: 'Book2',
+		publishedDate: 'May-2023',
+		description:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+		author: 'Hamza Ali'
+	},
+	{
+		name: 'Book3',
+		publishedDate: 'May-2023',
+		description:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+		author: 'Hamza Ali'
+	},
+	{
+		name: 'Book4',
+		publishedDate: 'May-2023',
+		description:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+		author: 'Hamza Ali'
+	},
+	{
+		name: 'Book1',
+		publishedDate: 'May-2023',
+		description:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+		author: 'Hamza Ali'
+	},
+	{
+		name: 'Book2',
+		publishedDate: 'May-2023',
+		description:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+		author: 'Hamza Ali'
+	},
+	{
+		name: 'Book3',
+		publishedDate: 'May-2023',
+		description:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+		author: 'Hamza Ali'
+	},
+	{
+		name: 'Book4',
+		publishedDate: 'May-2023',
+		description:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+		author: 'Hamza Ali'
+	}
+];
