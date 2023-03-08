97ea9f92262d0565931a356e47dd99462e012b2c
hamza ali <tanolihamzaali@gmail.com>
2023-03-09 02:12:31 +0500

    feat: Extracted the summary page as a seperate page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,27 +5,7 @@ import { Books } from './constants';
 import { IPreview } from './model';
 
 function App() {
-	return (
-		<div className="">
-			<div className="main-container flex flex-col gap-10 px-5">
-				<div className="flex justify-center items-center w-full h-[70px] border-b border-outline">
-					<h1 className="text-typography text-2xl">BriefBooks</h1>
-				</div>
-				<div className="w-full flex justify-center gap-5 ">
-					<Input
-						placeholder="Search for book!!"
-						onChange={(e: React.ChangeEvent<HTMLInputElement>) => {}}
-					/>
-					<Button onClick={(e: React.MouseEvent<HTMLButtonElement>) => {}}>Search</Button>
-				</div>
-				<div className="w-full px-20 flex gap-10 flex-wrap pb-10 justify-center">
-					{Books.map((data: IPreview) => {
-						return <Preview {...data} />;
-					})}
-				</div>
-			</div>
-		</div>
-	);
+	return <div></div>;
 }
 
 export default App;
---
+++ client/src/Pages/Summries.tsx
@@ -0,0 +1,26 @@
+import { Button, Input } from '../Components';
+import Preview from '../Components/Cards/Preview';
+import { Books } from '../constants';
+import { IPreview } from '../model';
+
+export default function Summery() {
+	return (
+		<div className="main-container flex flex-col gap-10 px-5">
+			<div className="flex justify-center items-center w-full h-[70px] border-b border-outline">
+				<h1 className="text-typography text-2xl">BriefBooks</h1>
+			</div>
+			<div className="w-full flex justify-center gap-5 ">
+				<Input
+					placeholder="Search for book!!"
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) => {}}
+				/>
+				<Button onClick={(e: React.MouseEvent<HTMLButtonElement>) => {}}>Search</Button>
+			</div>
+			<div className="w-full px-20 flex gap-10 flex-wrap pb-10 justify-center">
+				{Books.map((data: IPreview) => {
+					return <Preview {...data} />;
+				})}
+			</div>
+		</div>
+	);
+}
