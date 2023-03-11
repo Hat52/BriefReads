cd65330efd29105b2989fc2f9982f4a550f2011b
hamza ali <tanolihamzaali@gmail.com>
2023-03-11 10:57:18 +0500

    Summary read page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,10 +9,22 @@ function App() {
 	return (
 		<div className="">
 			<div className="flex gap-5 justify-center items-center h-screen w-screen flex-col">
-				<div className="header w-[80vw] h-[80px] bg-secondaryFaded"></div>
+				{/* <div className="header w-[80vw] h-[80px] bg-secondaryFaded"></div> */}
 				<div className="flex w-[80vw] gap-5">
-					<div className="w-[65vw] h-[70vh] bg-secondaryFaded rounded-sm"></div>
-					<div className="w-[15vw] gap-4 flex overflow-scroll scrollbar-hide flex-col h-[70vh] bg-transparent rounded-sm p-4 shadow-md shadow-secondary">
+					<div className="relative w-[65vw] p-20 h-[90vh] flex flex-col items-center justify-center bg-secondaryFaded rounded-sm">
+						<div className="absolute top-2">
+							<h1 className="text-[24px] text-typography font-bold">Book Name</h1>
+							<p className="text-center text-outline font-extralight">Book subtitles</p>
+						</div>
+						<div className="h-full overflow-scroll scrollbar-hide text-typography text-[18px] text-left">
+							{text}
+							{text}
+						</div>
+						<p className="absolute  bottom-5 right-5 text-outline text-bold capitalize underline">
+							Author Name
+						</p>
+					</div>
+					<div className="w-[15vw] gap-4 flex overflow-scroll scrollbar-hide flex-col h-[90vh] bg-transparent rounded-sm p-4 shadow-md shadow-secondary">
 						{Array(20)
 							.fill(0)
 							.map(() => (
@@ -44,3 +56,15 @@ const SummaryCard = () => {
 		</div>
 	);
 };
+
+const text = `What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+
+Where does it come from?
+Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`;
