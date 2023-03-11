b47a4aff1ba527f43e28f2f8918c6f465dd29315
hamza ali <tanolihamzaali@gmail.com>
2023-03-11 10:49:17 +0500

    Removed comment section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,8 +11,7 @@ function App() {
 			<div className="flex gap-5 justify-center items-center h-screen w-screen flex-col">
 				<div className="header w-[80vw] h-[80px] bg-secondaryFaded"></div>
 				<div className="flex w-[80vw] gap-5">
-					<div className="w-[15vw] h-[70vh] bg-secondaryFaded rounded-sm"></div>
-					<div className="w-[48vw] h-[70vh] bg-secondaryFaded rounded-sm"></div>
+					<div className="w-[65vw] h-[70vh] bg-secondaryFaded rounded-sm"></div>
 					<div className="w-[15vw] gap-4 flex overflow-scroll scrollbar-hide flex-col h-[70vh] bg-transparent rounded-sm p-4 shadow-md shadow-secondary">
 						{Array(20)
 							.fill(0)
