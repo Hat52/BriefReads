1af626527312aa6e54a96e7967ad3da2303b25b4
hamza ali <tanolihamzaali@gmail.com>
2023-03-11 11:15:05 +0500

    Summary preview card

---
+++ client/src/Components/Cards/SummaryCard.tsx
@@ -0,0 +1,22 @@
+import { AiOutlineLike, AiOutlineDislike } from 'react-icons/ai';
+import { ISummaryCard } from '../../model';
+
+const SummaryCard = ({ name, like, dislike }: ISummaryCard) => {
+	return (
+		<div className="w-full cursor-pointer px-2 py-1 shadow-sm bg-secondaryFaded shadow-outlineFaded h-max rounded-md hover:scale-105 transition-all duration-500">
+			<h6 className="font-semibold text-typography">{name}</h6>
+			<div className="flex text-outline justify-end gap-2 pointer-events-none">
+				<div className="flex items-center gap-1">
+					<span className="text-[10px]">{like}</span>
+					<AiOutlineLike />
+				</div>
+				<div className="flex items-center gap-1">
+					<span className="text-[10px]">{dislike}</span>
+					<AiOutlineDislike />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default SummaryCard;
